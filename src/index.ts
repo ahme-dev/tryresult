4b9b31c8b7a06f8175820de2fc7f1a7944d897f8
Ahmed K. A <ahmadkabdullah@pm.me>
2023-05-20 22:17:30 +0300

    FIX: Directly export functions in index file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-import { isError, tryAsync, trySync } from "./new";
-import { hasError, resultAll, resultAsync } from "./old";
-
-export default {
-	isError,
-	tryAsync,
-	trySync,
-	hasError,
-	resultAll,
-	resultAsync,
-};
+export { isError, tryAsync, trySync } from "./new";
+export { hasError, resultAll, resultAsync } from "./old";
